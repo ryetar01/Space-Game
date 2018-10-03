01320539d4f43417341a3ac291ff05c3db23206d
AzureAD\JonahTafuri <jonah.tafuri@baxter-academy.org>
2018-10-03 11:04:48 -0400

    Platformer camera.

--- Space Game/Assets/Scripts/platformerCamera.cs
+++ Space Game/Assets/Scripts/platformerCamera.cs
@@ -7,14 +7,27 @@ public class platformerCamera : MonoBehaviour {
     /*what needs to be done: need to start tracking the player once a or d is pressed and once they move a certain
       amount until they stop in which the camera will stop and fin*/
 
+    private Vector3 playerXYcameraZ;
     private GameObject player;
     public float playerDistanceFromCamera;
     public int yOffset;
+    public int xOffset;
+    public float panSpeed;
+    public int cameraTransitionOffsetX;
+    public int cameraTransitionOffsetY;
+    public bool correctMode = false;
+
+    //for the purpose of using the player transform once he activates the if statement
+    private Vector3 frozenPlayerTransform;
 
     // Use this for initialization
     void Start () {
         player = GameObject.Find("Player");
         yOffset = 1;
+        xOffset = -6;
+        panSpeed = 1;
+        cameraTransitionOffsetX = 3;
+        cameraTransitionOffsetY = 1;
     }
 
     // Update is called once per frame
@@ -24,17 +37,24 @@ public class platformerCamera : MonoBehaviour {
 
         if(player.GetComponent<platformerMovement>().isA || player.GetComponent<platformerMovement>().isD)
         {
-            playerDistanceFromCamera = player.transform.position.x - transform.position.x;
+            playerDistanceFromCamera = player.transform.position.x - transform.position.x + cameraTransitionOffsetX;
             //Vector3 offset = player.transform.position.x - transform.position.x;
 
-
-            if (playerDistanceFromCamera > 8 || playerDistanceFromCamera < -8)
+            if (correctMode)
             {
-                transform.position = new Vector3(player.transform.position.x, transform.position.y /*yOffset*/, -10);
-                /*if(player.GetComponent<Rigidbody2D>().IsSleeping()) 
+                frozenPlayerTransform = player.transform.position;
+                playerXYcameraZ = new Vector3(frozenPlayerTransform.x + cameraTransitionOffsetX, frozenPlayerTransform.y + cameraTransitionOffsetY, transform.position.z);
+                transform.position = Vector3.Lerp(transform.position, playerXYcameraZ, 0.1f);
+                if (Mathf.Abs(playerDistanceFromCamera) < 0.2f)
                 {
-                    
-                }*/
+                    correctMode = false;
+                }
+
+            } else if  (playerDistanceFromCamera > 8 || playerDistanceFromCamera < -5)
+            {
+                correctMode = true;
+                //transform.position = new Vector3(player.transform.position.x - xOffset, transform.position.y /*yOffset*/, -10);
+
             }
 
         }

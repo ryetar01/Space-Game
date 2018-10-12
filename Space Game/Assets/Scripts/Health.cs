346cf6286ca366a2d343b9495269eb7ff65e3e46
AzureAD\EvanNorzow <LeSkree@gmail.com>
2018-10-12 10:56:28 -0400

    Fixed Unity version

---
+++ Space Game/Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class health : MonoBehaviour {
+
+    public Slider myHealthBar;
+
+	// Use this for initialization
+	void Start () {
+        myHealthBar.value = 100;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (myHealthBar.value <= 0)
+        {
+        }*/
+    }
+
+    public void gotHit()
+    {
+        myHealthBar.value -= 5;
+    }
+
+
+}
---
+++ Space Game/Assets/Scripts/damage.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damage : MonoBehaviour
+{
+    private GameObject healthBar;
+    private GameObject player;
+    private Rigidbody2D playerRigidbody;
+    public int flinchForce;
+    public bool isFlinching = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerRigidbody = player.GetComponent<Rigidbody2D>();
+        healthBar = GameObject.Find("health");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator flinch(float a, float b)
+    {
+       playerRigidbody.AddForce(new Vector2(a, b), ForceMode2D.Impulse);
+        float oldDrag = playerRigidbody.drag;
+        playerRigidbody.drag = 0.9f;
+        isFlinching = true;
+        yield return new WaitForSeconds(0.175f);
+        playerRigidbody.drag = oldDrag;
+        playerRigidbody.velocity = Vector2.zero;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            healthBar.GetComponent<health>().gotHit();
+
+            //checking for if the player is using platformer
+            if (player.GetComponent<platformerMovement>() != null)
+            {
+                //checking player direction
+                if (player.GetComponent<platformerMovement>().isA == true)
+                {
+                    StartCoroutine(flinch(flinchForce, 0));
+                }
+                if (player.GetComponent<platformerMovement>().isD == true)
+                {
+                    StartCoroutine(flinch(-flinchForce, 0));
+                }
+            }
+
+            //checking for if the player is using topdown
+            if (player.GetComponent<topdownMovement>() != null)
+            {
+                //checking player direction
+                if (player.GetComponent<topdownMovement>().isW)
+                {
+                    StartCoroutine(flinch(0, -flinchForce));
+                }
+                if (player.GetComponent<topdownMovement>().isS == true)
+                {
+                    StartCoroutine(flinch(0, flinchForce));
+                }
+                if (player.GetComponent<topdownMovement>().isA == true)
+                {
+                    StartCoroutine(flinch(flinchForce, 0));
+                }
+                if (player.GetComponent<topdownMovement>().isD == true)
+                {
+                    StartCoroutine(flinch(-flinchForce, 0));
+                }
+            }
+        }
+    }
+}

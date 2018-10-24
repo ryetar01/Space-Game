8fb9a393dd0a75488ffe42f33e397e12f75ef645
AzureAD\JonahTafuri <jonah.tafuri@baxter-academy.org>
2018-10-24 10:39:35 -0400

    Trying to get walk and idle animation to work, it's gonna look broken but the script is what matters.

---
+++ space game 2/Space Game/Assets/Scripts/walkAnim2D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkAnim2D : MonoBehaviour {
+
+    private GameObject player;
+    public Animator playerAnimator;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        playerAnimator = player.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player.GetComponent<platformerMovement>().isA || player.GetComponent<platformerMovement>().isA == true){
+            playerAnimator.SetBool("ifMoving", true);
+        }
+        if (player.GetComponent<Rigidbody2D>().IsSleeping())
+        {
+            playerAnimator.SetBool("ifMoving", false);
+        }
+
+    }
+}

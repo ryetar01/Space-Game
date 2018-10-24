f86df1c7d5763ed026cf539014f2369ab6a06797
AzureAD\EvanNorzow <LeSkree@gmail.com>
2018-10-24 11:02:52 -0400

    Hopefully not breaking anything with this, just a fix.

---
+++ Space Game/Assets/Scripts/platformerCamera.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformerCamera : MonoBehaviour {
+
+    private Vector3 playerXYcameraZ;
+    private GameObject player;
+    public float playerDistanceFromCameraX;
+    public float playerDistanceFromCameraY;
+    public int yOffset;
+    public int xOffset;
+    public float panSpeed;
+    public int cameraTransitionOffsetX;
+    public int cameraTransitionOffsetY;
+    public bool correctMode = false;
+    public bool correctMode2 = false;
+
+
+    //for the purpose of using the player transform once he activates the if statement
+    private Vector3 frozenPlayerTransform;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player");
+        yOffset = 1;
+        xOffset = -6;
+        panSpeed = 1;
+        cameraTransitionOffsetX = 3;
+        cameraTransitionOffsetY = 1;
+    }
+
+    // Update is called once per frame
+
+    void Update () {
+       
+
+        if(player.GetComponent<platformerMovement>().isA || player.GetComponent<platformerMovement>().isD)
+        {
+            playerDistanceFromCameraX = player.transform.position.x - transform.position.x + cameraTransitionOffsetX;
+            playerDistanceFromCameraY = player.transform.position.y - transform.position.y + cameraTransitionOffsetY;
+
+            if (correctMode)
+            {
+                frozenPlayerTransform = player.transform.position;
+                playerXYcameraZ = new Vector3(frozenPlayerTransform.x + cameraTransitionOffsetX, frozenPlayerTransform.y + cameraTransitionOffsetY, transform.position.z);
+                transform.position = Vector3.Lerp(transform.position, playerXYcameraZ, 0.1f);
+                if (Mathf.Abs(playerDistanceFromCameraX) < 0.2f)
+                {
+                    correctMode = false;
+                }
+
+            } else if  (playerDistanceFromCameraX > 8 || playerDistanceFromCameraX < -5)
+            {
+                correctMode = true;
+            }
+
+            if (correctMode2)
+            {
+                frozenPlayerTransform = player.transform.position;
+                playerXYcameraZ = new Vector3(frozenPlayerTransform.x + cameraTransitionOffsetX, frozenPlayerTransform.y + cameraTransitionOffsetY, transform.position.z);
+                transform.position = Vector3.Lerp(transform.position, playerXYcameraZ, 0.05f);
+                if (Mathf.Abs(playerDistanceFromCameraY) < 0.2f)
+                {
+                    correctMode2 = false;
+                }
+
+            }
+            else if (playerDistanceFromCameraY > 4 || playerDistanceFromCameraY < -1)
+            {
+                correctMode2 = true;
+            }
+
+
+        }
+
+
+		
+	}
+}
+
+
---
+++ Space Game/Assets/Scripts/topdownMovement.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class topdownMovement : MonoBehaviour
+{
+    private Sprite faceView;
+    private Sprite sideView;
+    private Sprite backView;
+    public bool isW;
+    public bool isS;
+    public bool isA;
+    public bool isD;
+
+
+
+    //Inspector Variables
+    private float playerSpeed = 3; //speed player moves 
+
+    private void Start()
+    {
+        faceView = Resources.Load<Sprite>("Sprites/Front_Placeholder");
+        sideView = Resources.Load<Sprite>("Sprites/Side_Back_Placeholder");
+        backView = Resources.Load<Sprite>("Sprites/Side_Back_Placeholder");
+
+
+    }
+    void Update()
+    {
+        moveForward(); // Player Movement
+    }
+
+    private void moveForward()
+    {
+
+        if (Input.GetKey("s"))//Press up arrow key to move back on the Y AXIS
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
+            //playerSpriteRenderer.sprite = new Sprite(faceView);
+            GetComponent<SpriteRenderer>().sprite = faceView;
+            isS = true;
+            isW = false;
+            isA = false;
+            isD = false;
+        }
+        if (Input.GetKey("d"))//Press up arrow key to move forward on the X AXIS
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+            transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
+            GetComponent<SpriteRenderer>().sprite = sideView;
+            isD = true;
+            isW = false;
+            isA = false;
+            isS = false;
+        }
+        if (Input.GetKey("w"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+            transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
+            GetComponent<SpriteRenderer>().sprite = backView;
+            isW = true;
+            isD = false;
+            isA = false;
+            isS = false;
+        }
+        if (Input.GetKey("a"))//Press up arrow key to move back on the X AXIS
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 270);
+            transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
+            GetComponent<SpriteRenderer>().sprite = sideView;
+            isA = true;
+            isD = false;
+            isW = false;
+            isS = false;
+        }
+
+    }
+}

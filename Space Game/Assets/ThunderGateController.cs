41f598d825d553205af4cd3b6022969589b4f552
AzureAD\EvanNorzow <LeSkree@gmail.com>
2018-12-03 10:38:51 -0500

    Fixing the bullet prefab.

---
+++ Space Game/Assets/ThunderGateController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderGateController : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+    public Animator animator;
+
+    void Awake () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+     }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        StartCoroutine(End());
+    }
+    IEnumerator End()
+    {
+        animator.SetInteger("GateHealth", value: 1);
+        yield return new WaitForSeconds(0.2f); //duration of animation
+    }
+
+}

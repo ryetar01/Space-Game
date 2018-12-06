bd3923c183985320f308ad7768d71e0cb03bfcb2
AzureAD\JonahTafuri <jonah.tafuri@baxter-academy.org>
2018-12-06 10:44:19 -0500

    Big ass rat

---
+++ Space Game/Assets/Scripts/enemyPatrol.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyPatrol : MonoBehaviour {
+
+    public float enemySpeed;
+    private GameObject bigAssRatCollider;
+
+    // Use this for initialization
+    void Start () {
+        bigAssRatCollider = GameObject.Find("Collider");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(Vector3.right * Time.deltaTime * enemySpeed);
+
+        if (bigAssRatCollider.GetComponent<secondaryCollider>().hitWall ==  true)
+        {
+            Debug.Log("WEEEE");
+        }
+    }
+
+
+}
---
+++ Space Game/Assets/Scripts/secondaryCollider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secondaryCollider : MonoBehaviour {
+
+    public GameObject theBullet;
+    private GameObject player2;
+    private GameObject player;
+    public bool hitWall;
+
+    // Use this for initialization
+    void Start () {
+        player2 = GameObject.Find("Player2");
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collider2D col)
+    {
+        if (col.gameObject != player || col.gameObject != player2 || col.gameObject != theBullet)
+        {
+            hitWall = true;
+        }
+    }
+}

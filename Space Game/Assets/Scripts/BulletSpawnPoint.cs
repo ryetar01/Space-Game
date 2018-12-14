91d990c211653c40d95f449ec9ef6454972df57a
AzureAD\EvanNorzow <LeSkree@gmail.com>
2018-12-14 10:51:37 -0500

    Yote

--- Space Game/Assets/Scripts/BulletSpawnPoint.cs
+++ Space Game/Assets/Scripts/BulletSpawnPoint.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class BulletSpawnPoint : MonoBehaviour {
 
     public Vector3 offsetright;
-    public Space player;
     public GameObject parent;
-    public handleInput hi;
+    public TDInputHandler hi;
     public Vector3 offsetup;
     public Vector3 offsetdown;
     public Vector3 offsetleft;
+
 
 	void Start () {
         this.transform.localPosition = offsetright;
@@ -23,22 +23,22 @@ public class BulletSpawnPoint : MonoBehaviour {
 
         if (hi.s == true)//Press up arrow key to move back on the Y AXIS
         {
-            this.transform.SetPositionAndRotation(offsetdown, Quaternion.Euler(new Vector3(0, 0, 180)));
+            transform.SetPositionAndRotation(offsetdown, Quaternion.Euler(new Vector3(0, 0, 180)));
 
         }
         if (hi.d == true)//Press up arrow key to move forward on the X AXIS
         {
-            this.transform.SetPositionAndRotation(offsetright, Quaternion.Euler(new Vector3(0, 0, 90)));
+            transform.SetPositionAndRotation(offsetright, Quaternion.Euler(new Vector3(0, 0, 90)));
 
         }
         if (hi.w == true)//Press up arrow key to move forward on the Y AXIS
         {
-            this.transform.SetPositionAndRotation(offsetup, Quaternion.Euler(new Vector3(0, 0, 0)));
+            transform.SetPositionAndRotation(offsetup, Quaternion.Euler(new Vector3(0, 0, 0)));
 
         }
         if (hi.a == true)//Press up arrow key to move back on the X AXIS
         {
-            this.transform.SetPositionAndRotation(offsetleft, Quaternion.Euler(new Vector3(0, 0, 270)));
+            transform.SetPositionAndRotation(offsetleft, Quaternion.Euler(new Vector3(0, 0, 270)));
         }
     }
 }

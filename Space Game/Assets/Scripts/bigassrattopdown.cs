f2de99b88320d32c139333b0a13337e40d64eeef
beanslurper <jonah.tafuri@baxter-academy.org>
2019-01-25 12:48:18 -0500

    skate fast

--- Space Game/Assets/Scripts/bigassrattopdown.cs
+++ Space Game/Assets/Scripts/bigassrattopdown.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class bigassrattopdown : MonoBehaviour {
+
+    private Transform t;
+    private Transform player;
     private bool hit;
     public float enemySpeed;
     public bool hitEnemy;
-	// Use this for initialization
-	void Start () {
+    public float distanceToPlayer;
+    private bool assRatAttack;
+    // Use this for initialization
+
+
+    private void Awake()
+    {
+        t = this.transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Start () {
 
     }
 
@@ -19,13 +32,26 @@ public class bigassrattopdown : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        transform.parent.Translate(Vector3.up * Time.deltaTime * enemySpeed);
+        distanceToPlayer = float.Parse(Distance().ToString());
+        if(distanceToPlayer <= 10)
+        {
+            assRatAttack = true;
+        }
+        if(assRatAttack == true)
+        {
+            transform.parent.Translate(Vector3.up * Time.deltaTime * enemySpeed);
+        }
 
        if (hit == true)
         {
             transform.parent.Rotate(0, 0, Random.Range(90, 270));
             hit = false;
         }
+    }
+
+    private float Distance()
+    {
+        return Vector3.Distance(t.position, player.position);
     }
 
 

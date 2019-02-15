2849882df35a3aa30413f585ca5c41edf92a3b83
AzureAD\EvanNorzow <LeSkree@gmail.com>
2019-02-15 13:58:58 -0500

    Continuing work on the shifter.

--- Space Game/Assets/Planeshift.cs
+++ Space Game/Assets/Planeshift.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Planeshift : MonoBehaviour
 {
+    public GameObject player;
+    public gameManager GM;
+    public Vector2 Ppoint;
+    public Vector2 Tpoint;
+    public GameObject TDCamera;
+    public GameObject PCamera;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (GM.gamemode == true)
+        {
+            Object.Destroy(collision.gameObject);
+            GM.SpawnTopdown();
+        }
+        else
+        {
+            Object.Destroy(collision.gameObject);
+            GM.SpawnPlatformer();
+        }
     }
+
+
 }
---
+++ Space Game/Assets/gameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public bool gamemode;
+    public Planeshift Shift1;
+    public Planeshift Shift2;
+    public bool mainActive;
+    public bool catOn, catActive;
+    public bool owlOn, owlActive;
+    public bool moleOn, moleActive;
+    public bool chadOn, chadActive;
+     
+    
+    void Start()
+    {
+        gamemode = true;
+        //False is Platformer, Topdown is True.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnPlatformer()
+    {
+        
+    }
+
+    public void SpawnTopdown()
+    {
+        
+    }
+}

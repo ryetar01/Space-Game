d6326ea43b1a59bbc8cdf50379256eeb6f729aac
calebquatticci <35737110+calebquatticci@users.noreply.github.com>
2019-03-01 10:32:35 -0500

    space game

---
+++ Space Game/Assets/BulletDirection2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDirection2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+            transform.rotation = Quaternion.Euler(0, 0, 270);
+        
+    }
+}
--- Space Game/Assets/bulletdirection.cs
+++ Space Game/Assets/bulletdirection.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class bulletdirection : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-   
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.E))
+        {
             transform.rotation = Quaternion.Euler(0, 0, 180);
-        if (Input.GetKey(KeyCode.D))
+        }
+
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.E))
+        {
             transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+   
+       
     }
 
 }
